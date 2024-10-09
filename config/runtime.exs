1788126a98d7f07c77654b266e5bd11072081d90
Hermann Ólafsson <hermann.olafsson@devoteam.com>
2024-10-09 12:07:31 +0200

    resolve type o

--- config/runtime.exs
+++ config/runtime.exs
@@ -53,7 +53,7 @@ if config_env() == :prod do
   # https://hexdocs.pm/phoenix/Phoenix.Endpoint.html#socket/3-common-configuration
   config :sector7g, Sector7gWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    check_origin: (System.get_env("PHX_CHECK_ORIGIN") || "localhost,127.0.01") |> String.split(","), # TODO make this work with true/false
+    check_origin: (System.get_env("PHX_CHECK_ORIGIN") || "localhost,127.0.0.1") |> String.split(","), # TODO make this work with true/false
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
