e5f29b04bfc9f46581139e2b65cac8453ebf7761
Hermann Ólafsson <hermann.olafsson@devoteam.com>
2024-10-09 14:36:35 +0200

    basic promex setup

--- lib/sector7g/application.ex
+++ lib/sector7g/application.ex
@@ -8,19 +8,15 @@ defmodule Sector7g.Application do
   @impl true
   def start(_type, _args) do
     children = [
+      Sector7gWeb.Endpoint,
+      Sector7g.PromEx,
       Sector7gWeb.Telemetry,
       Sector7g.Repo,
       {Ecto.Migrator,
         repos: Application.fetch_env!(:sector7g, :ecto_repos)},
       {DNSCluster, query: Application.get_env(:sector7g, :dns_cluster_query) || :ignore},
       {Phoenix.PubSub, name: Sector7g.PubSub},
-      # Start the Finch HTTP client for sending emails
-      # {Finch, name: Sector7g.Finch},
-      # Start a worker by calling: Sector7g.Worker.start_link(arg)
-      # {Sector7g.Worker, arg},
-      # Start to serve requests, typically the last entry
       Sector7g.DaysSince,
-      Sector7gWeb.Endpoint
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
